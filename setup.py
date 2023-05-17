b9654028a012764e2c4d12480929ca7159d5367f
paul-shannon <paul.thurmond.shannon@gmail.com>
2023-05-17 10:48:15 -0700

    removed base64

--- setup.py
+++ setup.py
@@ -16,7 +16,6 @@ setuptools.setup(
     long_description_content_type='text/markdown',
     url='https://github.com/paul-shannon/slexil2',
     install_requires=[
-		'base64',
 		'formatting',
 		'logging',
 		'os',
