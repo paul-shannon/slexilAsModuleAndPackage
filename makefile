bbe550a572b359245399d52ff913e9c28cb7dc21
paul-shannon <paul.thurmond.shannon@gmail.com>
2023-05-17 09:58:34 -0700

    cleaned up setup.py

--- makefile
+++ makefile
@@ -1,5 +1,5 @@
 default:
-	@echo targets: venv install check test 
+	@echo targets: venv install intall-via-setup check test 
 
 venv:
 	@echo run: source ~/github/slexilAsModuleAndPackage/pySlexil/bin/activate
@@ -7,8 +7,14 @@ venv:
 install:
 	pip install . --upgrade
 
+install-via-setup:
+	python setup.py install
+
+twine:
+	./pySlexil/bin/twine  upload --repository-url https://test.pypi.org/legacy/ dist/*
+
 check:
 	ls -lat ~/github/slexilAsModuleAndPackage/pySlexil/lib/python3.10/site-packages/slexil*
  
 test:
-	(cd tests; 	make)
+	(cd tests; 	make test)
--- setup.py
+++ setup.py
@@ -26,27 +26,10 @@ setuptools.setup(
     package_dir={'': 'src'},
     packages=setuptools.find_packages(where='src'),
     classifiers=[
-        # see https://pypi.org/classifiers/
-        'Development Status :: 5 - Production/Stable',
-
-        'Intended Audience :: Developers',
-        'Topic :: Software Development :: Build Tools',
-
         'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.6',
-        'Programming Language :: Python :: 3.7',
-        'Programming Language :: Python :: 3.8',
-        'Programming Language :: Python :: 3.9',
-        'Programming Language :: Python :: 3.10',
-        'Programming Language :: Python :: 3 :: Only',
-        'License :: OSI Approved :: MIT License',
-        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 3.10'
     ],
-    python_requires='>=3.6',
-    # install_requires=['Pillow'],
-    extras_require={
-        'dev': ['check-manifest'],
-        # 'test': ['coverage'],
+    python_requires='>=3.10'
     },
 
 )
